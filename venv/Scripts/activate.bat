616b9c6b19e793d43c24a8889c347874cdad8317
siva vaishali b <siva04vaishali@gmail.com>
2024-12-01 18:32:59 +0530

    Resolved merge conflicts

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sinchana T\OneDrive\Desktop\Resume-screening-tool\venv
+set VIRTUAL_ENV=C:\Users\siva0\Resume-Screening-Tool\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
